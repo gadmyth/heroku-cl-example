b8bba4f0e4d43dff09ec9a8286ab2ce197399bc2
gadmyth <gadmyth@gmail.com>
2012-06-03 19:56:43 +0800

    m

--- example.asd
+++ example.asd
@@ -4,7 +4,7 @@
   :depends-on (#:hunchentoot
 	       #:cl-who
 	       #:postmodern
-	       #:md5-system
+	       #:md5
 	       )
   :components ((:file "package")
 	       (:module :src
--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -5,7 +5,7 @@
 (load (make-pathname :directory *build-dir* :name "md5-1.8.5/md5" :type "asd"))
 (load (make-pathname :directory *build-dir* :defaults "example.asd"))
 
-(ql:quickload :md5-system)
+(ql:quickload :md5)
 (ql:quickload :example)
 
 ;;; Redefine / extend heroku-toplevel here if necessary.
