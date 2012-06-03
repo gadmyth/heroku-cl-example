37530c957d0edd7fedac0ff6681cedc0d336dd2c
gadmyth <gadmyth@gmail.com>
2012-06-03 22:29:35 +0800

    m

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -1,12 +1,15 @@
 (in-package :cl-user)
 
+(export '*build-dir*)
+(print "build-dir:")
+(print *build-dir*)
+
+
 (print ">>> Building system....")
+
 
 (load (make-pathname :directory *build-dir* :name "md5-1.8.5/md5" :type "asd"))
 (load (make-pathname :directory *build-dir* :defaults "example.asd"))
-
-(print "build-dir:")
-(print *build-dir*)
 
 (ql:quickload :md5)
 (ql:quickload :example)
