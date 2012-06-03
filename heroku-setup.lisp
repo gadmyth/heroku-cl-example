561151e07a85125f9e94895b1da96e992102b68f
gadmyth <gadmyth@gmail.com>
2012-06-03 21:10:12 +0800

    m

--- heroku-setup.lisp
+++ heroku-setup.lisp
@@ -5,6 +5,8 @@
 (load (make-pathname :directory *build-dir* :name "md5-1.8.5/md5" :type "asd"))
 (load (make-pathname :directory *build-dir* :defaults "example.asd"))
 
+(format t "~A~%" *build-dir*)
+
 (ql:quickload :md5)
 (ql:quickload :example)
 
